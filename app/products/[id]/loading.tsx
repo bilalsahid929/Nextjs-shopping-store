2252516ba2c0690cb1911b438594039e22360cec
bilalshahid929 <bilalshahid929@gmail.com>
2023-11-06 02:58:40 +0400

    Add the skeleton

--- app/products/[id]/loading.tsx
+++ app/products/[id]/loading.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Skeleton } from "@/app/components";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="container mx-auto p-4 product-section">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="md:order-1">
+          <Skeleton height="20rem" />
+        </div>
+
+        <div className="md:order-2">
+          <Skeleton height="20rem" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
